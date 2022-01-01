a8f0146a5cf2850578a9d9f6e3b5c311804a2c82
Ilya_Evseev <evseev.i.a@bk.ru>
2022-01-01 19:50:16 +0500

    newscript

---
+++ Old Info Card/oldInfoCard.js
@@ -0,0 +1,64 @@
+const oldInfoCard = {
+     gameTextInterval: null,
+     openOldInfoCard(params, interfaceName) {
+          params = JSON.parse(params);
+          let houseTypes = ["Эконом класс", "Деревенски дом", "Средни класс", "Премиум класс", "Элитны дом", "Эконом класс", "VIP класс", "Квартира"];
+
+          let type = interfaceName == "Business" ? 1 : 0;
+          let name = type == 0 ? params[1] : params[2];
+          let roomAmount = type == 0 ? params[4] : 0;
+          let owner = type == 0 ? params[2] : params[3];
+          let price = type == 0 ? params[6] : params[5];
+          let rent = type == 0 ? params[5] : params[4];
+          let paramType = type == 0 ? houseTypes.indexOf(params[3].replace('й', '').replace('й', '').replace('+', '')) : params[1];
+
+          params = JSON.stringify([type, name, roomAmount, owner, price, rent, paramType]);
+
+          window.openInterface('InfoCard', params);
+          if (interfaceName == 'Appartament') setTimeout(() => {
+               interface('InfoCard').$el.children[0].children[1].children[0].children[0].innerHTML = `<span style="color:${owner.toLocaleLowerCase() == 'государство' ? 'hsl(120deg 100% 39%)' : 'FFFFFF'}">${owner}</span>`
+               interface('InfoCard').$el.children[0].children[0].children[1].children[0].children[0].innerHTML = `<span style="color:${owner.toLocaleLowerCase() == 'государство' ? 'hsl(120deg 100% 39%)' : 'FFFFFF'}">${name}</span>`
+          }, 50);
+     },
+     hookAndReplaceNewInfoCard() {
+          App.$children[0].$refs['Appartament'] = [{
+               close() {
+                    console.log('Закрыл новый интерфейс');
+
+                    clearInterval(oldInfoCard.gameTextInterval);
+                    interface('GameText').add(`[3,\"~w~Для взаимодействия нажмите ~g~alt\",500,0,0,0]`)
+                    window.closeInterface('InfoCard');
+               }
+          }];
+
+          App.$children[0].$refs['Business'] = [{
+               close() {
+                    console.log('Закрыл новый интерфейс');
+
+                    clearInterval(oldInfoCard.gameTextInterval);
+                    interface('GameText').add(`[3,\"~w~Для взаимодействия нажмите ~g~alt\",500,0,0,0]`)
+                    window.closeInterface('InfoCard');
+               }
+          }];
+
+          window.openInterface = new Proxy(window.openInterface, {
+               apply(target, thisArgs, args) {
+
+                    if (args[0] === 'Business' || args[0] === 'Appartament') {
+                         console.log('Показываю новый интерфейс', args);
+
+                         interface('GameText').add(`[3,\"~w~Для взаимодействия нажмите ~g~alt\",3000,0,0,0]`);
+                         oldInfoCard.gameTextInterval = setInterval(() => interface('GameText').add(`[3,\"~w~Для взаимодействия нажмите ~g~alt\",3000,0,0,0]`), 2000)
+                         oldInfoCard.openOldInfoCard(args[1], args[0]);
+                         return 0;
+                    }
+
+                    return Reflect.apply(target, thisArgs, args);
+               }
+          });
+
+
+     }
+}
+
+oldInfoCard.hookAndReplaceNewInfoCard();
